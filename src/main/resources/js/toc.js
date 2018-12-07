f2f9a4349e7b9629053cbffdddfd2dcc1a58212c
Damien Vitrac <dvitrac@pivotal.io>
2018-12-07 15:05:52 +0100

    Remove dynamic toc on mobile version

--- src/main/resources/js/toc.js
+++ src/main/resources/js/toc.js
@@ -3,8 +3,9 @@ if (toctitle != null) {
     var oldtoc = toctitle.nextElementSibling;
     var newtoc = document.createElement('div');
     newtoc.setAttribute('id', 'tocbot');
-    newtoc.setAttribute('class', 'js-toc');
-    oldtoc.parentNode.replaceChild(newtoc, oldtoc);
+    newtoc.setAttribute('class', 'js-toc desktop-toc');
+    oldtoc.setAttribute('class', 'mobile-toc');
+    oldtoc.parentNode.appendChild(newtoc);
     tocbot.init({
         contentSelector: '#content',
         headingSelector: 'h1, h2, h3, h4, h5',
