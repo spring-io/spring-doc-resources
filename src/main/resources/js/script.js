894d7db516c3db68ad951d8f9fce4a32963c0e16
Jay Bryant <jbryant@pivotal.io>
2020-01-27 13:43:12 -0600

    Add feedback (#21)
    * Adding a feedback widget for the reference docs
    
    I created the first draft of a feedback widget for the reference docs.
    
    * Added a project name field
    
    We'll need to be able to filter by project name, so I added a field for the project name.
    
    * Added a formatted date
    
    For the sake of display in the viewer application, I added a formatted date field.
    
    * Adding the necessary JavaScript libraries. Damien Vitrac caught that they were missing. Thanks, Damien.
    
    * Adding more items that Damien discovered I had forgotten. Thanks, Damien.
    
    * Adding more changes that Damien discovered were missing. In toc.js this time. Thanks, Damien.
    
    * Adding the images this time.
    
    * Review and optimisations
    
    * Added the ability to turn off the feedback widget
    
    The feedback widget does not appear unless an element with an ID of 'pageId' exists in the asciidoc file. The feedback widget needs an element with that ID, so that makes a sensible switch.
    
    * Cleaning up some unneeded bits
    
    The work Damien did made some things (jquery stuff) unnecessary. I also fixed a grammatical error.
    
    * CSS cleanup
    
    Damien made feedback.css be unnecessary, but that meant moving a CSS rule to a SASS file. I took care of that.
    
    * A few more changes as the result of testing
    
    I did some more testing (trying to be thorough) and caught a few more things. I made changes accordingly.
    
    Co-authored-by: Damien Vitrac <oodamien@users.noreply.github.com>

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -14,23 +14,39 @@ if (toctitle != null) {
         fixedSidebarOffset: 90,
         smoothScroll: false
     });
-    if (!path.endsWith("index.html") && !path.endsWith("/")) {
-        var link = document.createElement("a");
+    if (!path.endsWith('index.html') && !path.endsWith('/')) {
+        var link = document.createElement('a');
         if (document.getElementById('index-link')) {
-          indexLinkElement = document.querySelector('#index-link > p > a');
-          linkHref = indexLinkElement.getAttribute("href");
-          link.setAttribute("href", linkHref);
+            indexLinkElement = document.querySelector('#index-link > p > a');
+            linkHref = indexLinkElement.getAttribute('href');
+            link.setAttribute('href', linkHref);
         } else {
-          link.setAttribute("href", "index.html");
+            link.setAttribute('href', 'index.html');
         }
-        link.innerHTML = "<span><i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i></span> Back to index";
-        var block = document.createElement("div");
+        link.innerHTML = '<span><i class="fa fa-chevron-left" aria-hidden="true"></i></span> Back to index';
+        var block = document.createElement('div');
         block.setAttribute('class', 'back-action');
         block.appendChild(link);
         var toc = document.getElementById('toc');
         var next = document.getElementById('toctitle').nextElementSibling;
         toc.insertBefore(block, next);
     }
+}
+
+var starsNumber = 0;
+
+var feedbackHtml = '<div id="feedback-area" onmouseout="onStarMouseover(starsNumber)">' +
+  '<h1>Send feedback</h1>' +
+  getStarsComponent('stars') +
+  '</div>';
+
+if(document.getElementById('pageId') != null) {
+  var feedback = document.createElement('div');
+  feedback.innerHTML = feedbackHtml;
+
+  var toc = document.getElementById('toc');
+  var next = document.getElementById('toctitle').nextElementSibling;
+  toc.insertBefore(feedback, next);
 }
 
 var headerHtml = '<div id="header-spring">\n' +
@@ -104,10 +120,155 @@ var headerHtml = '<div id="header-spring">\n' +
     '</g>\n' +
     '</g>\n' +
     '</svg>\n' +
-    '\n' +
     '</h1>\n' +
     '</div>';
 
-var header = document.createElement("div");
+var header = document.createElement('div');
 header.innerHTML = headerHtml;
 document.body.insertBefore(header, document.body.firstChild);
+
+function getIssueURL() {
+    var metas = document.getElementsByTagName('meta');
+    for (var i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('name') === 'issue-url') {
+            return metas[i].getAttribute('content');
+        }
+    }
+    return '';
+}
+
+function getProjectName() {
+    var metas = document.getElementsByTagName('meta');
+    for (var i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('name') === 'project') {
+            return metas[i].getAttribute('content');
+        }
+    }
+    return '';
+}
+
+function getPageId() {
+  var pageIdElement = document.querySelector('#pageId > p');
+  return pageIdElement.innerHTML;
+}
+
+function getDateString() {
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+    var today = new Date();
+    var thisDay = today.getDate();
+    var thisMonth = monthNames[today.getMonth()];
+    var thisYear = today.getFullYear();
+    return thisDay + ' ' + thisMonth + ', ' + thisYear;
+}
+
+function getHeadings() {
+    var activeTocElement = toc.getElementsByClassName('is-active-li');
+    var headings = [];
+    if (activeTocElement && activeTocElement.length > 0) {
+        activeTocElement = activeTocElement[0];
+        headings.push({
+            text: activeTocElement.childNodes[0].textContent,
+            href: activeTocElement.childNodes[0].href
+        });
+        while (activeTocElement.parentElement.parentElement.className === 'toc-list-item') {
+            headings.push({
+                text: activeTocElement.parentElement.parentElement.childNodes[0].textContent,
+                href: activeTocElement.parentElement.parentElement.childNodes[0].href
+            });
+            activeTocElement = activeTocElement.parentElement.parentElement;
+        }
+    } else {
+        headings.push({
+            text: 'Top of document',
+            href: ''
+        })
+    }
+    return headings.reverse().map(function (value) {
+        if (value.href) {
+            return value.text + ' (' + value.href + ')';
+        }
+        return value.text;
+    }).join(' > ');
+}
+
+var urlIssue = getIssueURL();
+if (urlIssue) {
+    document.getElementById('ref-issue').setAttribute('href', urlIssue)
+} else {
+    document.getElementById('p-issue').style.display = 'none';
+}
+
+function getStarsComponent(id) {
+    return '<div class="stars" id="' + id + '">' +
+        '<span><a id="' + id + '1" class="star" onclick="onStarClick(1)" onmouseover="onStarMouseover(1)"></a></span>' +
+        '<span><a id="' + id + '2" class="star" onclick="onStarClick(2)" onmouseover="onStarMouseover(2)"></a></span>' +
+        '<span><a id="' + id + '3" class="star" onclick="onStarClick(3)" onmouseover="onStarMouseover(3)"></a></span>' +
+        '<span><a id="' + id + '4" class="star" onclick="onStarClick(4)" onmouseover="onStarMouseover(4)"></a></span>' +
+        '<span><a id="' + id + '5" class="star" onclick="onStarClick(5)" onmouseover="onStarMouseover(5)"></a></span>' +
+        '</div>';
+}
+
+function onStarMouseover(index) {
+    for (var i = 1; i <= 5; i++) {
+        var a = document.getElementById('stars' + i);
+        var b = document.getElementById('dialog-stars' + i);
+        var clazz = (i <= index) ? 'star active' : 'star';
+        if (a) {
+            a.className = clazz;
+        }
+        if (b) {
+            b.className = clazz;
+        }
+    }
+}
+
+function resetForm() {
+    starsNumber = 0;
+    document.getElementById('feedback-comment').value = '';
+    document.getElementById('feedback-email').value = '';
+    onStarMouseover(0);
+}
+
+function onStarClick(index) {
+    starsNumber = index;
+    MicroModal.show('modal-1', {disableScroll: true});
+}
+
+function submitFeedback(event) {
+    try {
+        event.preventDefault();
+        var parser = new UAParser();
+        var parserResult = parser.getResult();
+        var path = window.location.pathname;
+        var browserName = parserResult.browser.name;
+        var browserVersion = parserResult.browser.version;
+        var osName = parserResult.os.name;
+        var osVersion = parserResult.os.version;
+        var timestamp = new Date().getTime();
+        var obj = {
+            'starNumber': starsNumber,
+            'feedbackText': document.getElementById('feedback-comment').value,
+            'email': document.getElementById('feedback-email').value,
+            'projectName': getProjectName(),
+            'pageId': getPageId(),
+            'path': path,
+            'browserName': browserName,
+            'browserVersion': browserVersion,
+            'osName': osName,
+            'osVersion': osVersion,
+            'timestamp': timestamp,
+            'dateString': getDateString(),
+            'heading': getHeadings()
+        };
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://spring-docs-feedback.cfapps.io/feedback', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(obj));
+        resetForm();
+        MicroModal.close('modal-1');
+        alert('Your feedback has been sent.')
+    } catch (e) {
+        alert('An error occurred while sending your feedback.')
+    }
+}
