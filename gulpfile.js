ac78c614de16c620b8dc222f93a8936a95037bd0
Phillip Webb <pwebb@vmware.com>
2020-10-01 15:43:39 -0700

    User GitHub theme when testing

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ function render() {
                 'sectnums',
                 'source-highlighter=highlight.js',
                 'highlightjsdir=js/highlight',
-                'highlightjs-theme=color-brewer',
+                'highlightjs-theme=github',
                 'stylesdir=css',
                 'stylesheet=spring.css',
                 'docinfo=shared',
